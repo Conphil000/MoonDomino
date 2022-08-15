798d1d682fdf016b200da9b952694f62a51eb1da
Conor <Conphil000@github.com>
2022-08-14 21:23:43 -0500

    Made some changes to the hand class to accept string or domino, and converted the hand into a dictionary where the instance is the dictionary key and if it is usable is the value, neat

--- GAME.py
+++ GAME.py
@@ -13,8 +13,7 @@ import random
 class GAME:
     def __init__(self,hands,bone = None):
         self._players = hands
-        if bone != None:
-            self._bone = bone
+        self._bone = bone
             
     @staticmethod
     def CYCLE_TURN(turn):
@@ -27,16 +26,36 @@ class GAME:
     @staticmethod
     def RANDOM_MOVE(moves):
         return random.Random(123).choice(moves)
+    
     @staticmethod
     def HAND_WINNER(moves,lead_suit,trump = None):
+        
+        trump = 7 if trump == None else trump
+        
+        is_ace = [i.ACE() for i in moves]
+        is_trump = [i.TRUMP(trump) for i in moves]
+        is_suit = [str(lead_suit) in i.GET() for i in moves]
+        
+        # ace_trump > trump > ace > max_suit
+        
+        check_me = [(i,j,k) for i,j,k in zip(is_ace,is_trump,is_suit)]
+        print([i.GET() for i in moves])
+        print(check_me)
+        print(sum([1 if i[0] and i[1] else 0 for i in check_me]))
+        print(sum([1 if i[1] and i[2] else 0 for i in check_me]))
+        print(sum([1 if i[2] else 0 for i in check_me]))
+        
+        
+        # [1 if i[1] for i in check_me]
+        
         if trump == None:
-            
             ace = [i.ACE() for i in moves]
             
-            if sum(ace):
+            if sum(ace) > 0:
                 idx = ace.index(True)
+                print(idx)
                 if str(lead_suit) in moves[idx].GET():
-                    print(f'Ace {lead_suit} found')
+                    # print(f'Ace {lead_suit} found')
                     return idx
                 
             keep = [i.GET().replace(str(lead_suit),'') if str(lead_suit) in i.GET() else -999 for i in moves]
@@ -47,21 +66,40 @@ class GAME:
             # print(keep,idx)
             return idx
         else:
-            
             raise NotImplementedError
+            # ace = [i.ACE() for i in moves]
+            # trumps = [i.TRUMP(trump) for i in moves]
+            # print(trumps)
+            # if sum(trumps) > 0:
+                
+            #     print([i.GET() if j else -999 for i,j in zip(moves,trumps)])
+                
+            #     pass
+                
+            
+            
+            # if sum(ace) > 0:
+            #     idx = ace.index(True)
+            #     if (str(lead_suit) in moves[idx].GET()) and (sum(trump) == 0):
+            #         pass
+            
+            
+            
+            
+            
     
     def RANDOM_GAME(self,starter = 0):
         self._starter = starter
         self._turn = starter
         
         moves = []
+        
         player_moves = [-1] * len(self._players)
         player_pts = [0] * len(self._players)
         trump = None
         
         while sum(player_pts) < 7:
             # print(self._turn)
-            
             # print([i.GET() for i in self._players[self._turn].PLAYABLE()])
             
             m1 = self.RANDOM_MOVE(self._players[self._turn].PLAYABLE())
@@ -85,8 +123,8 @@ class GAME:
             moves.append(m3.GET())
             player_moves[self._turn] = m3
             
-            print(suit_lead)
-            print([i.GET() for i in player_moves])
+            # print(suit_lead)
+            # print([i.GET() for i in player_moves])
             
             # print('Winner IDX')
             # print([i.GET() for i in [m1,m2,m3]])
@@ -98,11 +136,19 @@ class GAME:
         # # Check for Winner
         
         print(moves)
-        print(suit_lead)
+        # print(suit_lead)
         print(player_pts)
-        
+        return player_pts
     
 if __name__ == '__main__':
+    
+    deck1 = DECK()
+    
+    player = HAND()
+    player.ADD_HAND(deck1[:7])
+    
+    player.KILL(player.PLAYABLE()[0])
+    player._hand
     
     deck = DECK()
     
@@ -110,29 +156,40 @@ if __name__ == '__main__':
         random.Random(123).shuffle(deck)
     
     def show_deck(deck):
-        print([i.GET() for i in deck])
+        return [i.GET() for i in deck]
     
     show_deck(deck)
     
     players = [HAND(),HAND(),HAND()]
     
-    players[0].SET_HAND(deck[:7])
-    players[1].SET_HAND(deck[7:14])
-    players[2].SET_HAND(deck[14:21])
+    players[0].ADD_HAND(deck[:7])
+    players[1].ADD_HAND(deck[7:14])
+    players[2].ADD_HAND(deck[14:21])
+    
     bone = deck[21]
     
     g1 = GAME(players,bone)
     
-   
+    assert g1.HAND_WINNER([DOMINO(3,3),DOMINO(2,2),DOMINO(1,5)], 1, None) == 2
+    assert g1.HAND_WINNER([DOMINO(3,3),DOMINO(2,2),DOMINO(1,5)], 3, None) == 0
+    
+    assert g1.HAND_WINNER([DOMINO(3,5),DOMINO(1,1),DOMINO(3,3)], 3, None) == 0
+    
+    assert g1.HAND_WINNER([DOMINO(5,5),DOMINO(5,3),DOMINO(1,1)], 1, 5) == 0
+    
+    assert g1.HAND_WINNER([DOMINO(3,3),DOMINO(2,2),DOMINO(1,5)], 3, 2) == 0
     
     for i in players:
-        show_deck(i.USABLE())
-        
-    print('and bone',bone.GET())
+        print(i.HAND(True))
+
+    print('and bone',[bone.GET()])
     print()
     print('Try a not so random game.')
     print()
-    g1.RANDOM_GAME()
+    
+    assert g1.RANDOM_GAME() == [3,1,3]
+    
+    
     
     # random.shuffle(deck.copy()) # Always will get the same deck.
     
--- HAND.py
+++ HAND.py
@@ -8,45 +8,62 @@ import random
 
 class HAND:
     def __init__(self,):
-        self._hand = []
-        self._usable = []
-    def ADD(self,DOMINO):
-        self._hand.append(DOMINO)
-        self._usable.append(1)
+        self._hand = {}
+        
+    def ADD_HAND(self,HAND):
+        self._hand = dict(zip(HAND,[True]*len(HAND)))
+        
+    def ADD_DOMINO(self,DOMINO): # Maybe will be used for Adding Bone?
+        self._hand[DOMINO] = True
+        
     def USABLE(self,):
         usable = []
-        for i in range(0,len(self._usable)):
-            if self._usable[i] == 1:
-                usable.append(self._hand[i])
+        for i in self._hand:
+            if self._hand[i]:
+                usable.append(i)
         return usable
+    
     def KILL(self,DOMINO):
-        for i in range(0,len(self._hand) ):
-            if self._hand[i] == DOMINO:
-                self._usable[i] = 0
-    def HAND(self,):
-        return [i for i in self.USABLE()]
-    def STARTER_HAND(self,):
-        return [i.GET() for i in self._hand]
+        if isinstance(DOMINO,str):
+            raise NotImplementedError
+            for i in self._hand:
+                if i.GET() == DOMINO:
+                    self._hand[i] = False
+        else:
+            self._hand[DOMINO] = False
+            
+    def PLAY(self,DOMINO):
+        self.KILL(DOMINO)
+        
+    def HAND(self,string = False):
+        if string:
+            return [i.GET() for i in self._hand.keys()]
+        else:
+            return list(self._hand.keys())
+    
     def PLAYABLE(self, lead = None):
         if lead == None:
             lead = 7
-        t1 = [i for i in self.HAND() if str(lead) in i.GET()]
-        if len(t1) > 0:
-            return t1
+            
+        play = []
+        
+        for i,j in self._hand.items():
+            if j:
+                if str(lead) in i.GET():
+                    play.append(i)
+                    
+        if len(play) > 0:
+            return play
         else:
-            return self.HAND()
-    def PLAY(self,DOMINO):
-        self.KILL(DOMINO)
-    def SET_HAND(self,hand):
-        self._hand = hand
-        self._usable = len(hand)*[1]
+            return [i for i,j in self._hand.items() if j == True]
+        
 if __name__ == '__main__':
     from DOMINO import DOMINO
     
     hand = HAND()
     
     for i in range(0,7):
-        hand.ADD(DOMINO(random.randint(1, 6),random.randint(1, 6)))
+        hand.ADD_DOMINO(DOMINO(random.randint(1, 6),random.randint(1, 6)))
         
     play = hand.USABLE()[3]
     print([i.GET() for i in hand.USABLE()])
@@ -59,23 +76,23 @@ if __name__ == '__main__':
     # Let's say someone played the 32, This is a 3 in this case. Trump is a 5.
     print('New Hand')
     hand2 = HAND()
-    hand2.ADD(DOMINO(1,6))
-    hand2.ADD(DOMINO(1,3))
-    hand2.ADD(DOMINO(3,6))
-    hand2.ADD(DOMINO(3,4))
+    hand2.ADD_DOMINO(DOMINO(1,6))
+    hand2.ADD_DOMINO(DOMINO(1,3))
+    hand2.ADD_DOMINO(DOMINO(3,6))
+    hand2.ADD_DOMINO(DOMINO(3,4))
         
     def show_deck(deck):
-        print([i.GET() for i in deck])
+        return [i.GET() for i in deck]
         
-    print(show_deck((hand2.HAND())))
+    print((hand2.HAND(True)))
     print('If 3 is lead you could play')
     print(show_deck(hand2.PLAYABLE(3)))
     assert len(hand2.PLAYABLE(3)) == 3
-    print(show_deck(hand2.HAND()))
+    print((hand2.HAND(True)))
     print('If 5 is lead you could play')
     print(show_deck(hand2.PLAYABLE(5)))
     assert len(hand2.PLAYABLE(5)) == 4
-    print(show_deck(hand2.HAND()))
+    print((hand2.HAND(True)))
     print('If 6 is lead you could play')
     print(show_deck(hand2.PLAYABLE(6)))
     assert len(hand2.PLAYABLE(6)) == 2
